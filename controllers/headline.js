8bcfe7934e33b48c11f32ed4b0799fa7002c4844
user <edwardhartman@macs-iMac-98.local>
2018-08-04 11:18:27 -0500

    yes

--- controllers/headline.js
+++ controllers/headline.js
@@ -1,18 +1,17 @@
+var express = require("express");
+var router = express.Router();
+var request = require("request");
+var cheerio = require("cheerio");
+var mongoose = require("mongoose");
+mongoose.Promise = Promise;
 
- var express = require("express");
- var router = express.Router();
- var request = require("request");
- var cheerio = require("cheerio");
- var mongoose = require("mongoose");
- mongoose.Promise = Promise;
-
- var Note = require("../models/Note.js");
- var Article = require("../models/Article.js");
- router.get("/", function(req, res){
+var Note = require("../models/Note.js");
+var Article = require("../models/Article.js");
+router.get("/", function(req, res){
    res.render("index");
  });
  
- router.get("/savedarticles", function(req, res){
+router.get("/savedarticles", function(req, res){
    Article.find({}, function(error, doc){
      if (error){
        console.log(error);
@@ -25,14 +24,14 @@
      }
    });
  });
- router.post("/scrape", function(req, res){
-   request("http://www.nytimes.com/", function(error, response, html){
-     var $ = cheerio.load(html);
-     var scrapedArticles = {};
-     $("article h2").each(function(i, element){
-       var result = {};
-       result.title = $(this).children("a").text();
-       console.log("what's the result title?" + result.title);
+router.post("/scrape", function(req, res){
+  request("http://www.nytimes.com/", function(error, response, html){
+    var $ = cheerio.load(html);
+    var scrapedArticles = {};
+    $("article h2").each(function(i, element){
+      var result = {};
+      result.title = $(this).children("a").text();
+      console.log("what's the result title?" + result.title);
        result.link = $(this).children("a".attr("href");
        scrapedArticles[i] = result;
 
@@ -45,65 +44,43 @@
    });
 
  });
- router.post("/save", function(req, res) {
-   console.log("This is the title: " + req.body.title);
-   var newArticleObject = {};
+router.post("/save", function(req, res) {
+  console.log("This is the title: " + req.body.title);
+  var newArticleObject = {};
 
   newArticleObject.title = req.body.title;
   newArticleObject.link = request.body.link;
-}
- })
-
-
-
-  newArticleObject.title = req.body.title;
-
-  newArticleObject.link = req.body.link;
-
   var entry = new Article(newArticleObject);
-
   console.log("We can save the article: " + entry);
-
-  // Now, save that entry to the db
-  entry.save(function(err, doc) {
-    // Log any errors
-    if (err) {
+  entry.save(function(err, doc){
+    if (err){
       console.log(err);
     }
-    // Or log the doc
     else {
       console.log(doc);
     }
   });
-
   res.redirect("/savedarticles");
 
-});
-
-router.get("/delete/:id", function(req, res) {
-
+ });
+router.get("/delete/:id", function(req, res){
   console.log("ID is getting read for delete" + req.params.id);
-
   console.log("Able to activate delete function.");
-
-  Article.findOneAndRemove({"_id": req.params.id}, function (err, offer) {
-    if (err) {
+  Article.findOneAndRemova({"_id": req.params.id}, function(err, offer){
+    if (err){
       console.log("Not able to delete:" + err);
-    } else {
+
+    } else{
       console.log("Able to delete, Yay");
     }
     res.redirect("/savedarticles");
   });
 });
-
-router.get("/notes/:id", function(req, res) {
-
+router.get("/notes/:id", function(req, res){
   console.log("ID is getting read for delete" + req.params.id);
-
   console.log("Able to activate delete function.");
-
-  Note.findOneAndRemove({"_id": req.params.id}, function (err, doc) {
-    if (err) {
+  Note.findOneAndRemove({"_id": req.params.id}, function(err, doc){
+    if (err){
       console.log("Not able to delete:" + err);
     } else {
       console.log("Able to delete, Yay");
@@ -112,54 +89,37 @@ router.get("/notes/:id", function(req, res) {
   });
 });
 
-// This will grab an article by it's ObjectId
-router.get("/articles/:id", function(req, res) {
-
+router.get("/articles/:id", function(req, res){
   console.log("ID is getting read" + req.params.id);
-
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
   Article.findOne({"_id": req.params.id})
-
   .populate('notes')
-
-  .exec(function(err, doc) {
-    if (err) {
+  .exec(function(err, doc){
+    if (err){
       console.log("Not able to find article and get notes.");
     }
     else {
-      console.log("We are getting article and maybe notes? " + doc);
+      console.log("We are getting article and maybe notes?" + doc);
       res.json(doc);
     }
   });
 });
-
-// Create a new note or replace an existing note
-router.post("/articles/:id", function(req, res) {
-
-  // Create a new note and pass the req.body to the entry
+router.post("/articles/:id", function(req, res){
   var newNote = new Note(req.body);
-  // And save the new note the db
-  newNote.save(function(error, doc) {
-    // Log any errors
+  newNote.save(function(error, doc){
     if (error) {
       console.log(error);
-    } 
+    }
     else {
-      // Use the article id to find it and then push note
-      Article.findOneAndUpdate({ "_id": req.params.id }, {$push: {notes: doc._id}}, {new: true, upsert: true})
-
+      Article.findOneAndUpdate({"_id": req.params.id}, {$push: {notes: doc._id}}, {new: true, upsert: true})
       .populate('notes')
-
       .exec(function (err, doc) {
         if (err) {
           console.log("Cannot find article.");
         } else {
-          console.log("On note save we are getting notes? " + doc.notes);
-          res.send(doc);
-        }
+          console.log("on note save we are getting notes? " + doc.notes);
+         }
       });
     }
   });
 });
-// Export routes for server.js to use.
 module.exports = router;
